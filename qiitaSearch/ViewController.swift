ba6c1ff8e45575a72ecc7de89f24f6df78a105e2
yukachilov <60588319+yukachilov@users.noreply.github.com>
2020-04-19 15:29:16 +0900

    Initial Commit

---
+++ qiitaSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  qiitaSearch
+//
+//  Created by 安部学 on 2020/04/19.
+//  Copyright © 2020 Manabu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
